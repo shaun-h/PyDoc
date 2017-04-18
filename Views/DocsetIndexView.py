c4bd7fcc84bbd6ea978aa000f4f02f0a82b67de6
Shaun Hevey <shaun@hevey.co>
2017-04-19 09:38:46 +1000

    added missing file

--- Views/DocsetIndexView.py
+++ Views/DocsetIndexView.py
@@ -4,13 +4,17 @@ import os
 from urllib.parse import quote
 
 class DocsetIndexView (object):
-	def __init__(self, docset, indexes, indexSelectCallback):
+	def __init__(self, docset, indexes, indexSelectCallback, docsetType):
 		self.data = indexes
 		self.docset = docset
 		self.indexSelectCallback = indexSelectCallback
+		self.docsetType = docsetType
 		
 	def tableview_did_select(self, tableview, section, row):
-		url = 'file://' + os.path.join(self.docset['path'], 'Contents/Resources/Documents', self.data[row]['path'])
+		if self.docsetType == 'docset':
+			url = 'file://' + os.path.join(self.docset['path'], 'Contents/Resources/Documents', self.data[row]['path'])
+		elif self.docsetType == 'cheatsheet':
+			url = 'file://' + os.path.join(self.docset.path, 'Contents/Resources/Documents', self.data[row]['path'])
 		url = url.replace(' ', '%20')
 		self.indexSelectCallback(url)
 		
@@ -29,15 +33,18 @@ class DocsetIndexView (object):
 		return cell
 	
 tv = ui.TableView()
-def get_view(docsets, indexes, indexSelectCallback):
+def get_view(docsets, indexes, indexSelectCallback, docsetType):
 	tv = ui.TableView()
 	w,h = ui.get_screen_size()
 	tv.width = w
 	tv.height = h
 	tv.flex = 'WH'
 	tv.name = 'PyDoc'
-	data = DocsetIndexView(docsets, indexes, indexSelectCallback)
+	data = DocsetIndexView(docsets, indexes, indexSelectCallback, docsetType)
 	tv.delegate = data
 	tv.data_source = data
-	tv.name = docsets['name']
+	if docsetType == 'docset':
+		tv.name = docsets['name']
+	elif docsetType == 'cheatsheet':
+		tv.name = docsets.name
 	return tv
